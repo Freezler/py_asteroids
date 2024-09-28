e7723b5bf45ad3a4a564331cf5936f010d607f82
Freezler <r.l.devries@live.nl>
2024-09-28 20:00:09 +0200

    ADD: forward/backward movement

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     pygame.init()
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    print("Starting asteroids!")
-    while pygame.init():
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          return
-      screen.fill((255, 255, 255))
-      pygame.display.flip()
-      clock.tick(60)
-      dt = clock.tick(60) / 1000
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
