29dfb64f4d460845fd355841a81ed5cfea65ca1f
Freezler <r.l.devries@live.nl>
2024-09-28 15:57:03 +0200

    ADD: circle as asteroid

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
 from constants import *
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     while pygame.init():
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
-      screen.fill((0, 0, 0))
+      screen.fill((255, 255, 255))
       pygame.display.flip()
+      clock.tick(60)
+      dt = clock.tick(60) / 1000
+
 
 
 
